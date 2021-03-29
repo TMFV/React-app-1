cc08b476394d8f3a7869f64ec611d08a89d08567
TMFV <denys.tymofieiev@gmail.com>
2021-03-29 23:51:22 +0300

    add local router

---
+++ src/components/Contacts/index.jsx
@@ -0,0 +1,8 @@
+const Contacts = () => {
+    return (
+        <div className="contacts">
+            <p>Contacts text</p>
+        </div>
+    );
+};
+export default Contacts;
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,9 +1,16 @@
 import Products from '../../pages/Products';
+import Contacts from '../../components/Contacts';
+
+console.dir(window.location);
+
 const Content = () => {
+    const { pathname } = window.location;
     return (
         <div className="content">
-            <Products />
+            {pathname === 'products' && <Products />}
+            {pathname === 'contacts' && <Contacts />}
         </div>
+        
     );
 };
 export default Content;
